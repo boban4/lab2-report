26e69c3841504ab309d0f28f6d5773cbc1f8cdc0
boban4 <garik.melikov@bk.ru>
2026-04-02 13:57:40 -0500

    Add comments

--- sources/hello.cpp
+++ sources/hello.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
-
 #include <string>
 
+//Main function
 int main()
 {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;
+//User name
+std::string name;
+
+// Request for name
+std::cout << "Enter your name: ";
+std::cin >> name;
+
+// Output greeting
+std::cout << "Hello world from " << name << std::endl;
+return 0;
 }
